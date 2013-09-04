e276ad901fa6e716c12c88df570737d9cd1850ff
googleask <scra.light@gmail.com>
2013-09-04 18:04:12 +0800

    update

--- ecstore/widgets/main_slide_/widgets.php
+++ ecstore/widgets/main_slide_/widgets.php
@@ -7,12 +7,12 @@
  */
  
 /*基础配置项*/
-$setting['author']='zinkwind@gmail.com';
+$setting['author']='tylerchao.sh@gmail.com';
 $setting['version']='v1.0';
 $setting['name']='首页主广告';
 $setting['order']=0;
-$setting['stime']='2012-08';
-$setting['catalog']='文章相关';
+$setting['stime']='2013-07';
+$setting['catalog']='广告相关';
 $setting['description'] = '展示模板使用的第一屏一级主广告';
 $setting['userinfo'] = '';
 $setting['usual']    = '1';
